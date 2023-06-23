ec343db420f83191b681484823c795d722da78e3
Amador Bueno <97048472+amadorbr27@users.noreply.github.com>
2023-06-23 17:07:34 -0300

    update: abort

--- main.py
+++ main.py
@@ -242,14 +242,16 @@ class RecoveryInterface(QMainWindow):
 
 
     def perform_abort(self):
-        logs = self.recovery_mode.RM_Abort(self.transaction)
+        current_transaction = str(self.combobox_transactions.currentText())
+        current_object_transaction = [T for T in self.transactions if f'T{T.id}' == current_transaction]
+        logs = self.recovery_mode.RM_Abort(current_object_transaction[0])
         for log in logs:
             self.log_memory_display.append(log)
             time.sleep(1)
             self.log_disk_display.append(log)
 
         if 'write_item' in self.transaction.steps:
-            filtered_logs = [log for log in logs if log.split(', ')[0] == 'write_item' and log.split(', ')[1] == f'T{self.transaction.id}']
+            filtered_logs = [log for log in logs if log.split(', ')[0] == 'write_item' and log.split(', ')[1] == f'T{current_object_transaction[0].id}']
             data_item = self.transaction.data_item
             new_value = filtered_logs[0].split(', ')[-1]
             self.update_db_table(self.dict_dropdown[data_item], new_value)
